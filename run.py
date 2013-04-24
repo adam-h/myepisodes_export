b6222a9d85974f7bb8a942d6211f08c5c1ed6c4a
Henry Jenkins <henry@henryjenkins.name>
2013-04-24 13:18:16 +1200

    Minor fixes
    Added https for trakt
    Fixed scraping of season and episode numbers from myepisodes
    Separate out my episodes user and password from code
    Added collection updating
    Tidied up README
    Replaced tabs with spaces (sorry).

--- run.py
+++ run.py
@@ -5,56 +5,78 @@ import tvdb_api, urllib2, json
 TRAKT_KEY = '##TRAKT_API_KEY##'
 TRAKT_USER = '##TRAKT_USER##'
 TRAKT_PASS = '##TRAKT_PASS##'
+MYEP_USER = '##MYEP_USER##'
+MYEP_PASS = '##MYEP_PASS##'
 
 tvdb = tvdb_api.Tvdb()
 
 trakt_pass_sha1 = sha1(TRAKT_PASS).hexdigest()
 
-me = MyEpisodes('##MYEP_USER##', '##MYEP_PASS##')
+me = MyEpisodes(MYEP_USER, MYEP_PASS)
 me.login()
 me.get_show_list()
 
 watchlist = []
 
 for show in me.show_list:
-	try:
-		tvdb_data = tvdb[show['name']]
-	except:
-		print "FAIL - Could not get TVDB ID for %s" % (show['name'])
-		continue
+    try:
+        tvdb_data = tvdb[show['name']]
+    except:
+        print "FAIL - Could not get TVDB ID for %s" % (show['name'])
+        continue
 
-	watchlist.append({'tvdb_id': tvdb_data['id'], "title": tvdb_data['seriesname']})
+    watchlist.append({'tvdb_id': tvdb_data['id'], "title": tvdb_data['seriesname']})
 
-	episodes = me.get_seen_episodes(show["id"])
+    collection_episodes = me.get_collection_episodes(show["id"])
 
-	trakt_data = {
-		"username": TRAKT_USER,
-		"password": trakt_pass_sha1,
-		"tvdb_id": tvdb_data['id'],
-		"title": tvdb_data['seriesname'],
-		"episodes": episodes
-	}
+    trakt_data = {
+        "username": TRAKT_USER,
+        "password": trakt_pass_sha1,
+        "tvdb_id": tvdb_data['id'],
+        "title": tvdb_data['seriesname'],
+        "episodes": collection_episodes
+    }
 
-	req = urllib2.Request('http://api.trakt.tv/show/episode/seen/' + TRAKT_KEY, json.dumps(trakt_data), {'content-type': 'application/json'})
-	f = urllib2.urlopen(req)
-	status = json.loads(f.read())
+    req = urllib2.Request('https://api.trakt.tv/show/episode/library/' + TRAKT_KEY, json.dumps(trakt_data), {'content-type': 'application/json'})
+    f = urllib2.urlopen(req)
+    status = json.loads(f.read())
 
-	if(status['status'] == 'success'):
-		print "OK - %s - %s" % (tvdb_data['seriesname'], status['message'])
-	else:
-		print "FAIL - %s - %s" % (tvdb_data['seriesname'], status['message'])
+    if(status['status'] == 'success'):
+        print "OK - Updated catalogue of %s - %s" % (tvdb_data['seriesname'], status['message'])
+    else:
+        print "FAIL - Could not update catalogue of %s - %s" % (tvdb_data['seriesname'], status['message'])
+
+
+    seen_episodes = me.get_seen_episodes(show["id"])
+
+    trakt_data = {
+        "username": TRAKT_USER,
+        "password": trakt_pass_sha1,
+        "tvdb_id": tvdb_data['id'],
+        "title": tvdb_data['seriesname'],
+        "episodes": seen_episodes
+    }
+
+    req = urllib2.Request('http://api.trakt.tv/show/episode/seen/' + TRAKT_KEY, json.dumps(trakt_data), {'content-type': 'application/json'})
+    f = urllib2.urlopen(req)
+    status = json.loads(f.read())
+
+    if(status['status'] == 'success'):
+        print "OK - Updated seen status of %s - %s" % (tvdb_data['seriesname'], status['message'])
+    else:
+        print "FAIL - Could not update seen status of %s - %s" % (tvdb_data['seriesname'], status['message'])
 
 trakt_data = {
-	"username": TRAKT_USER,
-	"password": trakt_pass_sha1,
-	"shows": watchlist
+    "username": TRAKT_USER,
+    "password": trakt_pass_sha1,
+    "shows": watchlist
 }
 
-req = urllib2.Request('http://api.trakt.tv/show/watchlist/' + TRAKT_KEY, json.dumps(trakt_data), {'content-type': 'application/json'})
+req = urllib2.Request('https://api.trakt.tv/show/watchlist/' + TRAKT_KEY, json.dumps(trakt_data), {'content-type': 'application/json'})
 f = urllib2.urlopen(req)
 status = json.loads(f.read())
 
 if(status['status'] == 'success'):
-	print "OK - Updated Watchlist"
+    print "OK - Updated Watchlist"
 else:
-	print "FAIL - Could not update Watchlist"
+    print "FAIL - Could not update Watchlist"
